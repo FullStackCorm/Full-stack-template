b6f535d4bab80c07dbea4f4a04d8d13e9ae935f8
Dev-Corinne <corinne.m.dev@gmail.com>
2022-06-22 21:11:04 -0400

    add middleware

--- server.js
+++ server.js
@@ -21,12 +21,13 @@ MongoClient.connect(dbConnectionString)
     })
 
 
-
-
     // MIDDLEWARE //
 
 app.set('view engine', 'ejs')
-
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
 
     // PORT = 8000 //
