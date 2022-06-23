402f0c0e4bc1b86425747b81a3fe89bd16237ef4
Dev-Corinne <corinne.m.dev@gmail.com>
2022-06-22 20:27:54 -0400

    add most recent changes

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// Require dependencies
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
-// middleware requirements //
+
+// Require middleware //
+
+// Declare variables //
+let db, 
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'star-wars-quotes',
+    collection 
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`All your base are belong to us. You are on the way to destruction.`)
+        db = client.db(dbName)
+        collection = db.collection('quotes')
+    })
